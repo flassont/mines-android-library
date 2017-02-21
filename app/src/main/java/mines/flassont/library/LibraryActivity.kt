4e0710204b41345a3cb22015c64f145c15789c4a
Florian Lassont <florian.lassont@gmail.com>
2017-02-21 20:18:43 +0100

    Initial Android + Kotlin setup

---
+++ app/src/main/java/mines/flassont/library/LibraryActivity.kt
@@ -0,0 +1,12 @@
+package mines.flassont.library
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LibraryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_library)
+    }
+}
